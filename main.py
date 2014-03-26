7338cd0b51531831bdb41234a31d9b0954884bc1
tvkwoodlands <tvkwoodlands@gmail.com>
2014-03-26 18:26:29 -0500

    Merge should be good now; Removed pollcron

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import logging
 
 import boto
 from boto.mturk.connection import MTurkConnection
-from launchHIT import launchHIT, rejectTurker, approveTurker
+from launchHIT import launchHIT
 from google.appengine.ext import db
 
 #----------------------------- Config ----------------------------------------#
@@ -31,7 +31,7 @@ class Drawing(db.Model):
     hitID = db.StringProperty(default='x', indexed=False)
     strokeAdded = db.BooleanProperty(default=False, indexed=False)
     #added
-    payment = db.FloatProperty(default='0.00', indexed=False)
+    payment = db.FloatProperty(default=0.00, indexed=False)
     description = db.StringProperty(default='x', indexed=False)
 
 class Stroke(db.Model):
@@ -85,19 +85,16 @@ class ViewDrawing(webapp2.RequestHandler):
         '''
         lines = []
         drawing = db.get(drawing_id)
-        #q = db.GqlQuery("SELECT lines FROM Stroke WHERE counter=:1",drawing)
-        q = db.GqlQuery("SELECT lines FROM Stroke")
-        strokesList = list(q)
-        sortedStrokesList = sorted(strokesList, key=lambda strokesList: strokesList.datetime)
-        sortedStrokesList.reverse()
+        q = db.GqlQuery("SELECT * FROM Stroke")
 
-        #lines = json.dumps([json.loads(stroke.lines[0]) for stroke in sortedStrokesList])
-        lines = ''
-        linesList = []
-        for stroke in sortedStrokesList:
-            if strokes.counter == drawing:
-                linesList.append(ast.literal_eval(stroke.lines[0]))
-        lines = json.dumps(linesList)
+        lines = []
+        for stroke in q:
+            if stroke.counter.key() == drawing.key():
+                for line in stroke.lines:
+                    lines.append(json.loads(line))
+        lines = json.dumps(lines)
+
+        #print lines
 
         context = {"drawing_id":drawing_id,"lines":lines}
         template = JINJA_ENVIRONMENT.get_template('view.html')
@@ -144,18 +141,14 @@ class DrawingPage(webapp2.RequestHandler):
         '''
         lines = []
         drawing = db.get(drawing_id)
-        q = db.GqlQuery("SELECT lines FROM Stroke")
-        strokesList = list(q)
-        sortedStrokesList = sorted(strokesList, key=lambda strokesList: strokesList.datetime)
-        sortedStrokesList.reverse()
+        q = db.GqlQuery("SELECT * FROM Stroke")
 
-        #lines = json.dumps([json.loads(stroke.lines[0]) for stroke in sortedStrokesList])
-        lines = ''
-        linesList = []
-        for stroke in sortedStrokesList:
-            if strokes.counter == drawing:
-                linesList.append(ast.literal_eval(stroke.lines[0]))
-        lines = json.dumps(linesList)
+        lines = []
+        for stroke in q:
+            if stroke.counter.key() == drawing.key():
+                for line in stroke.lines:
+                    lines.append(json.loads(line))
+        lines = json.dumps(lines)
 
         context = {'lines':lines, 'drawing_id':drawing_id}
         template = JINJA_ENVIRONMENT.get_template('drawing.html')
@@ -171,7 +164,7 @@ class DrawingPage(webapp2.RequestHandler):
         dataSent = json.loads(self.request.body)
 
         print 'number of lines = ', len(dataSent)
-        if len(dataSent) != 0:
+        if len(dataSent) > 0:
             drawing.strokeAdded = True
             drawing.put()
 
@@ -190,6 +183,99 @@ class ThanksPage(webapp2.RequestHandler):
         template_values = {}
         template = JINJA_ENVIRONMENT.get_template('thanks.html')
         self.response.write(template.render(template_values))
+        
+class Poll(webapp2.RequestHandler):
+    def get(self):
+        print 'XXXXX  CRON v6  XXXXX'
+        try:
+            #if there is anything in reviewableHits, handle it
+            reviewableHits = mtc.get_reviewable_hits()
+            #from here down, assume that there is a reviewable hit in reviewableHits
+            #may run many times (however many lines have been drawn and submitted since last poll)
+            for hit in reviewableHits:
+                worker_id = None
+                ass_id = None
+                hitID = hit.HITId
+                assignments = mtc.get_assignments(hit.HITId)
+                #only runs once "guaranteed"-ish
+                for assignment in assignments:
+                    worker_id = assignment.WorkerId
+                    ass_id = assignment.AssignmentId
+                #get the list of blocked ID's from the datastore
+                #query = db.GqlQuery("SELECT * FROM Drawing WHERE hitID = :1", hitID)
+                query = db.GqlQuery("SELECT * FROM Drawing")
+                #check if the ID of the person awaiting approval is in the list
+                for drawing in query:
+                    #filter the drawings by hitID (since we're not doing this in the query anymore)
+                    if drawing.hitID != hitID:
+
+                        #reject the worker if no stroke was added to the image
+                        if drawing.strokeAdded == False:
+                            print 'worker did not do work, relaunch hit' #no reason to block, just extra overhead without helping prevent them in future
+                            mtc.reject_assignment(ass_id, 'You did not follow directions: you must draw a stroke in order to be paid')
+                            mtc.dispose_hit(hitID)
+
+                            newHit = launchHIT(mtc, str(drawing.key()), float(drawing.payment), str(drawing.description))
+                            print 'new hit launched'
+                            #save over the old hit id with the new one
+                            drawing.hitID = newHit[0].HITId
+                            drawing.put()
+
+                        #if so, reject
+                        elif worker_id in drawing.blockedList:
+                            print 'worker is blocked'
+                            #TODO: get list of all strokes with this drawing, and throw out the most recent
+                            q = db.GqlQuery("SELECT lines FROM Stroke")
+                            strokesList = list(q)
+                            sortedStrokesList = sorted(strokesList, key=lambda strokesList: strokesList.datetime)
+                            sortedStrokesList.reverse()
+                            #for i in xrange(1, len(q)):
+                            for stroke in sortedStrokesList:
+                                print stroke
+                                result = db.delete(stroke)
+                                print result
+                                break
+
+                            print 'lines deleted'
+                            mtc.reject_assignment(ass_id, 'You can only do this job once per drawing')
+                            mtc.dispose_hit(hitID)
+                            #change
+                            newHit = launchHIT(mtc, str(drawing.key()), float(drawing.payment), str(drawing.description))
+                            print 'new hit launched'
+                            #save over the old hit id with the new one
+                            drawing.hitID = newHit[0].HITId
+                            drawing.put()
+                        #otherwise, approve the work and add that name to the list of blocked
+                        else:
+                            mtc.approve_assignment(ass_id)
+                            mtc.dispose_hit(hitID)
+                            drawing.blockedList.append(str(worker_id))
+                            #increment the drawing counter and save it
+                            drawing.count+=1
+                            #if the count of the drawing that person drew to is not done, put out a new HIT
+                            if drawing.count < drawing.strokeLimit:
+                                print 'count: ', drawing.count
+                                print 'limit: ', drawing.strokeLimit
+                                newHit = launchHIT(mtc, str(drawing.key()), float(drawing.payment), str(drawing.description))
+                                #save over the old hit id with the new one
+                                drawing.hitID = newHit[0].HITId
+                                print '++++++++++++++++++++++++++++++++++++++launched another'
+                            else:
+                                print 'drawing finished'
+                                drawing.finished = True
+
+                            #save all the new drawing info
+                            drawing.put()
+
+        except Exception as ex:
+            print 'API call failed!'
+            template = "an exception of type {0} occured. arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print message
+
+
+        finally:
+            print 'success'
 
 #--------------------------------- Routes ------------------------------------#
 
@@ -197,6 +283,7 @@ app = webapp2.WSGIApplication([
     ('/dashboard', Dashboard),
     ('/new', NewDrawing),
     ('/thanks', ThanksPage),
+    ('/polling', Poll),
     ('/view/([^/]+)', ViewDrawing),
     ('/([^/]+)', DrawingPage)
 ], debug=True)
